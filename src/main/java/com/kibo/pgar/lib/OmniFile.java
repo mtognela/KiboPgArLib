2260f95ae53a2227417e9bba0c3747e6b87afba3
Mattia Tognela <tognelamattia@protonmail.com>
2025-06-06 11:24:48 +0200

    renamed:    src/main/java/com/kibo/pgar/lib/UniFile.java -> src/main/java/com/kibo/pgar/lib/OmniFile.java

--- src/main/java/com/kibo/pgar/lib/OmniFile.java
+++ src/main/java/com/kibo/pgar/lib/OmniFile.java
@@ -5,14 +5,14 @@ import java.io.File;
 /**
  * Extension of {@link File} that adds cross-platform utilities.
  */
-public class UniFile extends File {
+public class OmniFile extends File {
 
     /**
      * Constructs a {@code UniversalFile} from a sequence of path components.
      * 
      * @param args Path segments to join with the system's file separator.
      */
-    public UniFile(String... args) {
+    public OmniFile(String... args) {
         super(String.join(File.separator, args));
     }
 
