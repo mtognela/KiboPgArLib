da6b6144e177b067101b5076f489dea0bd503fbc
Mattia Tognela <tognelamattia@protonmail.com>
2025-06-09 08:17:07 +0200

    Clean up

--- src/main/java/com/kibo/pgar/lib/Files/OmniFile.java
+++ src/main/java/com/kibo/pgar/lib/Files/OmniFile.java
@@ -22,21 +22,5 @@ public final class OmniFile {
     public static File of(String... args) {
         return new File(String.join(File.separator, args));
     }
-
-
-    /**
-     * Constructs a {@code File} from a sequence of path components.
-     * 
-     * @param path     Path segments to join with the system's file separator.
-     * @param fileName the file name
-     * @return the constructed File
-     */
-    public static File of(String[] path, String fileName) {
-        String[] fullPath = new String[path.length + 1];
-        System.arraycopy(path, 0, fullPath, 0, path.length);
-
-        fullPath[path.length] = fileName;
-        return new File(String.join(File.separator, fullPath));
-    }
-
+    
 }
